3145b07e12c52dad5e8c9ef5ab06142967d0ed9d
Hoa <danghuyhoa@gmail.com>
2019-12-13 14:33:36 +0700

    add provider module

---
+++ src/app/providers/provider-add/provider-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-add',
+  templateUrl: './provider-add.component.html',
+  styleUrls: ['./provider-add.component.css']
+})
+export class ProviderAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/providers/providers.module.ts
+++ src/app/providers/providers.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProviderListComponent } from './provider-list/provider-list.component';
+import { ProviderAddComponent } from './provider-add/provider-add.component';
+import {RouterModule, Routes} from '@angular/router';
 
-
+const routers: Routes = [
+  {
+    path: '',
+    component: ProviderListComponent
+  },
+  {
+    path: 'add',
+    component: ProviderAddComponent
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [ProviderListComponent, ProviderAddComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routers)
   ]
 })
 export class ProvidersModule { }
